2727ab16fae55c55406720e93f1744d455255c03
Steffen Holter <steffenholter@gmail.com>
2018-11-20 21:11:30 +0400

    Aggregation components

--- WebApp/Functions.py
+++ WebApp/Functions.py
@@ -528,6 +528,7 @@ def prep_for_D3_global(pre_proc_file,all_data_file,samples,bins_centred,position
                       "Installment Trades w/ Balance",
                       "Bank Trades w/ High Utilization Ratio",
                       "% Trades w/ Balance"]
+
 	pre_data = pd.read_csv(pre_proc_file).values
 	all_data = pd.read_csv(all_data_file,header=None).values[:,1:]
 
@@ -595,3 +596,97 @@ def prep_for_D3_global(pre_proc_file,all_data_file,samples,bins_centred,position
 	return final_data
 
 
+
+
+def prep_for_D3_aggregation(pre_proc_file,all_data_file,samples,bins_centred,positions,transform):
+
+	names = ["External Risk Estimate", 
+                      "Months Since Oldest Trade Open",
+                      "Months Since Last Trade Open",
+                      "Average Months in File",
+                      "Satisfactory Trades",
+                      "Trades 60+ Ever",
+                      "Trades 90+ Ever",
+                      "% Trades Never Delq.",
+                      "Months Since Last Delq.",
+                      "Max Delq. Last 12M",
+                      "Max Delq. Ever",
+                      "Total Trades",
+                      "Trades Open Last 12M",
+                      "% Installment Trades",
+                      "Months Since Most Recent Inq",
+                      "Inq Last 6 Months",
+                      "Inq Last 6 Months exl. 7 days",
+                      "Revolving Burden",
+                      "Installment Burden",
+                      "Revolving Trades w/ Balance:",
+                      "Installment Trades w/ Balance",
+                      "Bank Trades w/ High Utilization Ratio",
+                      "% Trades w/ Balance"]
+                      
+	pre_data = pd.read_csv(pre_proc_file).values
+	all_data = pd.read_csv(all_data_file,header=None).values[:,1:]
+
+	final_data = []
+
+	
+	for s in samples:
+
+		s -= 1
+		single_dict_list = []
+		for i in range(all_data.shape[1]):
+			result = {}
+			result["name"] = names[i]
+			result["incr"] = 0 
+
+			if pre_data[s][1] > 0.5:
+				result["dec"] = 1
+			else:
+				result["dec"] = 0
+
+			val = all_data[s][i].round(0)
+			change = val
+
+	        # -- Identify Anchors --
+			for an in range(5,9):
+				col = pre_data[s][an]
+				if (i == col):
+					result["anch"] = 1
+
+			# -- Find Change -- 
+			for a in range(9,14):
+				col = pre_data[s][a]
+				if (i == col):
+
+					new_sample_ind = int(transform[str(s)])
+					idx = positions[new_sample_ind][col]
+					increments = pre_data[s][a+5]
+					change = bins_centred[i][int(idx+increments)]
+
+
+
+			max_bin = np.max(bins_centred[i])
+			min_bin = np.min(bins_centred[i])
+
+			if (min_bin == -1):
+				min_bin = 0
+
+			if (max_bin < 10):
+				max_bin = 10
+
+			scl_val = ((val-min_bin)/(max_bin-min_bin)).round(2)
+			scl_change = ((change-min_bin)/(max_bin-min_bin)).round(2)
+
+			if (scl_val < 0 ):
+				scl_val = 0
+			if (scl_change < 0):
+				scl_change = 0
+
+			result["scl_val"] = float(scl_val)
+			result["scl_change"] = float(scl_change)
+
+			single_dict_list.append(result)
+			
+		final_data.append(single_dict_list)
+
+	return final_data
