2442c97a666eddac458c755a537054727e155e22
Oscar Gomez <oscar.gomezq@hotmail.com>
2019-07-03 16:31:13 +0400

    changed acc calculation and test_train splitting
    We were splitting train test by first 80% and 20% in order, now it is random order
    This will change acc for the FICO model so that one we need to leave as is (bc of preprocessed data), but we should change it to this for the general version

--- WebApplication/model.py
+++ WebApplication/model.py
@@ -2,10 +2,8 @@ import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 from sklearn import svm
-from utils import prepare_for_analysis
-
-
 
 class ModelError(Exception):
     pass
@@ -15,54 +13,47 @@ class SVM_model():
 
 		self.model = None
 
-		# -- Seperate --
+		# -- Separate --
 		self.y = target
 		scaler = StandardScaler()
 		self.X = scaler.fit_transform(data)
 
-		# -- Needs to be retained for inserting new samples
+		# -- Needs to be retained for inserting new samples --
 		self.mean = scaler.mean_
 		self.scale = scaler.scale_
 
-		self.num_samples , self.num_attributes = self.X.shape
+		self.num_samples, self.num_attributes = self.X.shape
 
 		# -- Split Training/Test -- 
-		self.X_tr = self.X[:int(0.8*self.num_samples)]
-		self.X_test = self.X[int(0.8*self.num_samples):]
-
-
-		self.y_tr = self.y[:int(0.8*self.num_samples)]
-		self.y_test = self.y[int(0.8*self.num_samples):]
-
+		self.X_tr, self.X_test, self.y_tr, self.y_test = train_test_split(
+		self.X, self.y, test_size=0.2, random_state=11)
 
 	def train_model(self,c_val=1):
 		self.model = svm.SVC(kernel='linear', C=c_val, probability=True)
-
 		self.model.fit(self.X_tr,self.y_tr.reshape(self.y_tr.shape[0],))
-
 
 	def test_model(self, X_ts=None, y_ts=None):
 		if (not self.model):
 			raise ModelError("Train Model First")
 
-		train_pred = self.model.predict(self.X_tr)
-		test_pred = self.model.predict(self.X_test)
+		acc_train = self.model.score(self.X_tr, self.y_tr)
+		acc_test = self.model.score(self.X_test, self.y_test)
 
-		acc_train = round((np.mean(train_pred.reshape(train_pred.shape[0],1) == self.y_tr)* 100),2)
-		acc_test = round((np.mean(test_pred.reshape(test_pred.shape[0],1) == self.y_test)* 100),2)
-
-		# print("Training Accuracy:", acc_train, '%')
+		print("Training Accuracy:", acc_train, '%')
 		print("Test Accuracy:", acc_test, '%')
 
-	def __scaled_row(self,row):
+
+	def __scaled_row(self, row):
 	    scld = []
 	    for k in range(row.shape[0]):
 	        scld.append((row[k] - self.mean[k])/self.scale[k])
 	    scld = np.array(scld)
+	    scld2 = np.array([(row[k] - self.mean[k])/self.scale[k] for k in range(row.shape[0])])
 	    
+	    print(scld, scld2)
 	    return np.array(scld)
 
-	def run_model(self,sample):
+	def run_model(self, sample):
 		sample = self.__scaled_row(sample)
 		if (not self.model):
 			raise ModelError("Train Model First")
@@ -84,6 +75,5 @@ class SVM_model():
 
 
  
-
 
 
