fb0fa9f837a579649f3fefb8d45fac0f0f85a794
5teffen <steffenholter@gmail.com>
2020-08-21 21:02:14 +0300

    Removed package

--- WebApplication/utils.py
+++ WebApplication/utils.py
@@ -5,9 +5,6 @@ from sklearn import preprocessing
 from operator import itemgetter
 
 from scipy.stats import norm, gaussian_kde, entropy
-import tensorflow as tf
-import seaborn as sns
-import copy
 
 import matplotlib.pyplot as plt
 
